a33510635f47761e3318824083eece5e24f49208
rPortelas <remy.portelas@gmail.com>
2019-06-06 14:14:45 +0200

    simplify gmm

--- param_env_utils/imgep_utils/gmm.py
+++ param_env_utils/imgep_utils/gmm.py
@@ -19,7 +19,7 @@ class EmpiricalLearningProgress():
     def get_lp(self, goal, competence):
         interest = 0
         if len(self.interest_knn) > 5:
-            # compute learning progress for new goal
+            # compute learning progre   ss for new goal
             dist, idx = self.interest_knn.nn_y(goal)
             # closest_previous_goal = previous_goals[idx]
             closest_previous_goal = self.interest_knn.get_y(idx[0])
@@ -38,7 +38,7 @@ class InterestGMM():
     def __init__(self, mins, maxs, n_components=None, seed=None):
         self.seed = seed
         if not seed:
-            self.seed = np.random.randint(np.random.randint(42,424242))
+            self.seed = np.random.randint(42,424242)
         self.mins = mins
         self.maxs = maxs
         self.random_goal_generator = Box(np.array(mins), np.array(maxs), dtype=np.float32)
@@ -48,7 +48,7 @@ class InterestGMM():
         self.goals_lps = []
         self.fit_rate = 250
         self.nb_random = 250
-        self.random_goal_ratio = 0.1
+        self.random_goal_ratio = 0.2
         self.window = 250
         self.potential_ks = np.arange(1,11,1)
 
@@ -99,7 +99,7 @@ class InterestGMM():
                 self.lp_stds.append(covar[-1,-1])
             for _ in range(n_samples):
                 # sample gaussian
-                idx = proportional_choice(self.lp_means, eps=0.1)
+                idx = proportional_choice(self.lp_means, eps=0.0)
                 # sample goal in gaussian, without forgetting to remove learning progress dimension
                 new_goal = np.random.multivariate_normal(self.gmm.means_[idx], self.gmm.covariances_[idx])[:-1]
                 new_goals.append(np.clip(new_goal, self.mins, self.maxs))
@@ -112,29 +112,3 @@ class InterestGMM():
     def dump(self, dump_dict):
         dump_dict.update(self.bk)
         return dump_dict
-
-
-
-
-
-
-
-
-if __name__=="__main__":
-    # Generate some data
-    X, y_true = make_blobs(n_samples=400, centers=4,
-                           cluster_std=0.60, random_state=0)
-    X = X[:, ::-1] # flip axes for better plotting
-    rng = np.random.RandomState(13)
-    X_stretched = np.dot(X, rng.randn(2, 2))
-    Xmoon, ymoon = make_moons(200, noise=.05, random_state=0)
-
-    gmm16 = InterestGMM([0,0],[1,1], n_components=16)
-    #plot_gmm(gmm16, Xmoon, label=False)
-    #plt.show(block=False)
-    #plt.pause(1.0)
-    gmm16.update(Xmoon, [0.]*len(Xmoon))
-    Xnew = gmm16.sample_goal(n_samples=400)
-    plt.scatter(Xnew[:, 0], Xnew[:, 1])
-    plt.show(block=False)
-    plt.pause(2.0)
